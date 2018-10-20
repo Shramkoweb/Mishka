72b286eb01753afe2ff16e3c0b8ec364db56964a
Shramkoweb <shramko.web@yahoo.com>
2018-10-20 13:02:53 +0300

    Настройка автоматизации минификации CSS & Images

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -14,6 +17,9 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
@@ -31,4 +37,20 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 7 }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.svgo({
+        plugins: [
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe(gulp.dest("source/img/test"));
+
+});
 gulp.task("start", gulp.series("css", "server"));
