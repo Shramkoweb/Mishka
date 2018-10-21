c435ece2bf24cf4d0aee0132ad0783d70cf53bed
Shramkoweb <shramko.web@yahoo.com>
2018-10-21 11:06:52 +0300

    Изменил название минифицированого css

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task("css", function () {
     ]))
     .pipe(gulp.dest("build/css"))
     .pipe(csso())
-    .pipe(rename("style.min.css"))
+    .pipe(rename("style-min.css"))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
