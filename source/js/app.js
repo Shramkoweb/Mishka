b6432d0f225bcc6c0cbd867c17ebc36935e3dc83
Shramkoweb <shramko.web@yahoo.com>
2018-10-22 12:15:26 +0300

    Добавил класс для мадального окан и JS для его активации

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,15 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+var special = document.querySelector(".special");
+var modal = document.querySelector(".modal");
 
-navMain.classList.remove('main-nav--no-js');
-navMain.classList.add('main-nav--closed');
+navMain.classList.remove("main-nav--no-js");
+navMain.classList.add("main-nav--closed");
 
-navToggle.addEventListener('click', function () {
-    navMain.classList.toggle('main-nav--closed');
+navToggle.addEventListener("click", function () {
+  navMain.classList.toggle("main-nav--closed");
+})
+
+special.addEventListener("click", function () {
+  modal.classList.add("modal--opened");
 })
